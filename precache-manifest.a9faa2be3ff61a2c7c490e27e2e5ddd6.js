6d0ed282714b9bcbec0feb620ed2f159f919c401
Zhuchara32 <vovazhuchara@gmail.com>
2021-12-22 14:56:48 +0200

    deploy

---
+++ precache-manifest.a9faa2be3ff61a2c7c490e27e2e5ddd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f2289eef234fd2568f",
+    "url": "/htmlEdditor/css/app.96f31d76.css"
+  },
+  {
+    "revision": "5ad6e65e460cf76d1c944d1ca89b54f3",
+    "url": "/htmlEdditor/index.html"
+  },
+  {
+    "revision": "f7f2289eef234fd2568f",
+    "url": "/htmlEdditor/js/app.5a75f921.js"
+  },
+  {
+    "revision": "a6d8763b995b40d5d75c",
+    "url": "/htmlEdditor/js/chunk-vendors.9d8f5569.js"
+  },
+  {
+    "revision": "72a8dd65757c68620731b74b0cf01253",
+    "url": "/htmlEdditor/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/htmlEdditor/robots.txt"
+  }
+]);
